8d1e2321e310ec96858f3b99d56e9f510e17f674
Cory Carte <7457451+corycarte@users.noreply.github.com>
2022-07-23 08:09:52 -0400

    Initial Commit

---
+++ SeeFood-Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeeFood-Swift
+//
+//  Created by Cory Carte on 7/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeeFood-Swift/SeeFood_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFood_SwiftApp.swift
+//  SeeFood-Swift
+//
+//  Created by Cory Carte on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SeeFood_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
